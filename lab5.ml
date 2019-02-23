c64353f644a59aa5823252c42ecefc8d3e6b72cb
Taehwan Kim <taehwankim@dhcp-10-250-41-149.harvard.edu>
2019-02-23 15:08:07 -0500

    finished lab

--- lab5.ml
+++ lab5.ml
@@ -92,7 +92,7 @@ channels. You'll want to use Simple and RGB as the value constructors
 in this new variant type.
 ......................................................................*)
 
-type color = Simple | RGB of int * int * int ;;
+type color = Simple of color_label | RGB of int * int * int ;;
 
 (* There is an important assumption about the RGB values that
 determine whether a color is valid or not. The RGB type presupposes an
@@ -141,7 +141,7 @@ exception Invalid_color of string ;;
 
 let validated_rgb (col : color) : color =
   match col with
-  | Simple -> col
+  | Simple _ -> col
   | RGB(r, g, b) -> if (r >= 0 && r <= 255
                     && g >= 0 && g <= 255
                     && b >= 0 && b <= 255) then col else raise (Invalid_color "invalid") ;;
@@ -177,12 +177,20 @@ below are some other values you might find helpful.
      75 |   0 | 130 | Indigo
     240 | 130 | 240 | Violet
 ......................................................................*)
-(*
-let convert_to_rgb (col : color) : (int * int * int) =
-  match col with
-  | RGB(r, g, b) -> (r, g, b)
-  | Simple -> ;; *)
 
+let convert_to_rgb (c : color) : int * int * int =
+  match c with
+  | RGB (r, g, b) -> (r, g, b)
+  | Simple x ->
+    match x with
+    | Red -> (255,   0,   0)
+    | Crimson -> (164,  16,  52)
+    | Orange ->  (255, 165,   0)
+    | Yellow ->  (255, 255,   0)
+    | Green ->   (  0, 255,   0)
+    | Blue ->    (  0,   0, 255)
+    | Indigo ->  ( 75,   0, 130)
+    | Violet ->  (240, 130, 240) ;;
 (*======================================================================
 Part 2: Dates as a record type
 
@@ -246,14 +254,21 @@ Exercise 8: Create a validated_date function that raises Invalid_date if
 the invariant is violated, and returns the date if valid.
 ......................................................................*)
 
-(* exception Invalid_date of string ;;
+exception Invalid_date of string ;;
 
-let validated_date ({year, month, day} : date) : date =
-  if y < 0 then raise Invalid_date "non-positive year" else
-  if m > 12 || m < 0 then raise Invalid_date "invalid month" else
-    match m with
-    |
- *)
+let validated_date ({year; month; day} as date) : date =
+  if year < 0 then raise (Invalid_date "negative year")
+  else
+    let leap = (year mod 4 = 0 && year mod 100 <> 0) || year mod 400 = 0 in
+    let max_days =
+      match month with
+      | 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31
+      | 4 | 6 | 9 | 11 -> 30| 2 -> if leap then 29 else 28
+      | _ -> raise (Invalid_date "bad month") in
+    if day > max_days then raise (Invalid_date "day too large")
+    else if day < 1 then raise (Invalid_date "day too small")
+    else date ;;
+
 
 (*======================================================================
 Part 3: Family trees as an algebraic data type
@@ -266,7 +281,7 @@ Exercise 9: Define a person record type. Use the field names "name",
 "favorite", and "birthdate".
 ......................................................................*)
 
-type person = NotImplemented ;;
+type person = { name : string; favorite : color; birthdate : date } ;;
 
 (* Let's now do something with these person values. We'll create a
 data structure that allows us to model simple familial relationships.
@@ -304,9 +319,9 @@ and returns a Single. If you completed the validity functions that
 ensure the invariants are preserved for color and date, use them here
 as well.
 ......................................................................*)
-
-let new_child =
-  fun _ -> failwith "new_child not implemented" ;;
+let new_child (name : string) (col : color) (birth : date) : family =
+  Single { name;
+           favorite = validated_rgb col;birthdate = validated_date birth } ;;
 
 (*......................................................................
 Exercise 11: Write a function that allows a person to marry in to a
@@ -317,8 +332,11 @@ is already made up of a married couple?
 
 exception Family_Trouble of string ;;
 
-let marry =
-  fun _ -> failwith "marry not implemented" ;;
+let marry (fam : family) (spouse : person) : family =
+  match fam with
+  | Single per -> Family (per, spouse, [])
+  | Family _ -> raise (Family_Trouble ("cannot add "
+                                       ^ spouse.name ^ " to a couple")) ;;
 
 (*......................................................................
 Exercise 12: Write a function that accepts two families, and returns
@@ -329,15 +347,18 @@ assumptions provided in the type definition of family to determine how
 to behave in corner cases.
 ......................................................................*)
 
-let add_to_family =
-  fun _ -> failwith "add_to_family not implemented" ;;
+let add_to_family (fam : family) (child : family) : family =
+  match fam with
+  | Single _ -> raise (Family_Trouble "singles don't have children")
+  | Family (p1, p2, children) -> Family (p1, p2, child :: children) ;;
 
 (*......................................................................
 Exercise 13: Complete the function below that counts the number of
 people in a given family. Be sure you count all spouses and children.
 ......................................................................*)
-
-let count_people =
-  fun _ -> failwith "count_people not implemented" ;;
+let rec count_people (fam: family) : int =
+  match fam with
+  | Single _ -> 1
+  | Family (_, _, c) -> 2 + List.fold_left (+) 0 (List.map count_people c) ;;
 
 
